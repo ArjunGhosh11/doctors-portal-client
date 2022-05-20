0f3188d39846f9f3df096d22c4192484ffb091f5
ArjunGhosh11 <ghosh.rj11@gmail.com>
2022-05-20 14:55:59 +0600

    services added

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -1,5 +1,6 @@
 import { format } from 'date-fns';
 import React, { useEffect, useState } from 'react';
+import Service from './Service';
 
 const AvailableAppointments = ({ date }) => {
     const [services, setServices] = useState([]);
@@ -12,6 +13,14 @@ const AvailableAppointments = ({ date }) => {
     return (
         <div>
             <h4 className='text-xl text-secondary text-center'>Available Appointments on {format(date, 'PP')}</h4>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                {
+                    services.map(service => <Service
+                        key={service._id}
+                        service={service}
+                    ></Service>)
+                }
+            </div>
         </div>
     );
 };
